08cbe80b3110a28b3f9d207dc58a7f6939abebd6
Theanonymous007 <137682649+Theanonymous007@users.noreply.github.com>
2026-09-15 15:37:40 +0530

    Update coder.py

--- coder.py
+++ coder.py
@@ -74,21 +74,19 @@ def coder_agent(plan: dict, architecture: dict):
         """
 
 
-        try:
-            response = llm.invoke(prompt).content
+      try:
+    response = llm.invoke(prompt).content
 
-            except RateLimitError:
-            time.sleep(10)
+except RateLimitError:
+    time.sleep(10)
 
-            try:
-                response = llm.invoke(prompt).content
+    try:
+        response = llm.invoke(prompt).content
 
-                except RateLimitError:
+    except RateLimitError:
         return {
             "error": "Groq rate limit reached. Please wait a minute and try again."
         }
-
-
         # Clean unwanted markdown if present
         cleaned = response.replace("```", "").strip()
 
